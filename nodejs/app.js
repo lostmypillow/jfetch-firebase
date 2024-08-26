60b36af4a1dc35cd224e77ce4d728f7eda474c85
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-08-26 22:16:57 +0800

    added cors

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,9 +1,11 @@
 const https = require("https");
 const express = require("express");
 const app = express();
+var cors = require("cors")
 const PORT = process.env.PORT || 3000;
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
 app.use(express.json());
+app.use(cors())
 app.use(
   express.urlencoded({
     extended: true,
